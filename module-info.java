181ff457dffe9ad4b84bedc9dd39b3892c8b6300
lejone18 <126075892+lejone18@users.noreply.github.com>
2023-02-25 15:57:17 +0200

    Delete module-info.java

--- module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.increasedecrease {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.increasedecrease to javafx.fxml;
-    exports com.example.increasedecrease;
-}
